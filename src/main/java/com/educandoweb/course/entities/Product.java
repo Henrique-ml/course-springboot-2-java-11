9e210afd9fad7fff49d86dd0b37411fc1d02278c
Henrique Lira <henrique.melhado@hotmail.com>
2021-01-13 21:03:34 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -11,17 +11,23 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+// Mapeamento objeto relacional do JPA
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
-	// Associação com a classe Product
+	// Ainda não se fez associação com a classe Order pois ainda não existe a classe OrderItem
+	
+	// Associação com a classe Category
 	
 	// Como ainda faltam coisas (provisório) o JPA não está reconhecendo a coleção Set, implementar essa Annotation
 	// - @Transient: impedir que o JPA tente interpretar isso
@@ -31,17 +37,22 @@ public class Category implements Serializable {
 	// Será feito com Set, que representa um conjunto e garante que não terá um Product com mais de uma ocorrência da mesma categoria...
 	// ...ou seja, o MESMO produto não pode ter a MESMA Category MAIS DE UMA VEZ
 	// - new HashSet<>(): garantir que a coleção comece VAZIA e INSTANCIADA e não NULA
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		
+		// Não se coloca coleções em um construtor pois já está se instanciando uma. Nesse caso a coleção "categories"
 	}
-
+	
 	public Long getId() {
 		return id;
 	}
@@ -57,12 +68,34 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 	
-	// Não se cria um método SET para coleções para elas não serem substituidas por uma outra nova coleção
+	public Set<Category> getCategories() {
+		return categories;
+	}
 
 	@Override
 	public int hashCode() {
@@ -80,7 +113,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
