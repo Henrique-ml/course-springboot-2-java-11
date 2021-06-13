dd5ce817304121f8bf446631fdb3a9f2fb6856ef
Henrique Lira <henrique.melhado@hotmail.com>
2021-06-13 17:17:47 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,17 +45,15 @@ public class UserResource {
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	
-	// <Void>: pois a resposta dessa requisição não retornará nenhum corpo
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-		
-		// Ao requisitar o método de exclusão de um objeto, será retornado o código 500...
-		// ... pois um objeto "User", no banco de dados, tem objetos "Order" associado a ele, os quais ficariam sem o objeto "User"...
-		// ... assim, perderíamos a integridade do nosso banco de dados
-		
-		// Por enquanto, nesta aula, não será implementado esta lógica de deleção com um objeto associado ao objeto "User"
-		// Consiguiremos deletar apenas novos objetos "User" que forem inseridos sem associações atreladas à eles
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);		
+	}
+	
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -32,4 +32,25 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		
+		// - .getOne(): instancia um objeto "User" mas sem utilizar o banco de dados, deixando este objeto monitorado pelo JPA...
+		// ... para trabalharmos com ele e só posteriormente efetuar alguma operação com o banco de dados...
+		// ... ou seja, ele só prepara o objeto
+		// Sendo melhor do que utilizarmos o método do Repository "findById()" que necessariamente vai ao bd consultar um objeto com dado ID
+		
+		// - entity: um objeto monitorado pelo JPA
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		
+		// Nem todos os atributos do objeto "User" aceitaremos que sejam atualizados, como o ID e a senha, por exemplo
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
